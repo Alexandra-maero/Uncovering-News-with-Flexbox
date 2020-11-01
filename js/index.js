19c873939d1d77875dec0277d3368fda48ff884e
Alexandra Maero <alexandramaero12@gmail.com>
2020-10-31 21:25:17 -0300

    attempt to add text from js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,22 @@
-var name = document.getElementById('name')
-var form = document.getElementById('form')
+const form = document.getElementById('form')
+const name = document.getElementById('name')
+const password = document.getElementById('password')
+const age = document.getElementById('age')
+const adress = document.getElementById('adress')
+const postal = document.getElementById('postal')
+const email = document.getElementById('email')
+const repeat = document.getElementById('repeat')
+const phone = document.getElementById('phone')
+const city = document.getElementById('city')
+const dni = document.getElementById('dni')
 
-form.addEventListener('submit', runEvent)
-
-function runEvent(e){
-    window.onload = load
+window.onload = function(e){
     e.preventDefault()
-    if (name.length < 6){
-        messages.push('The name requires more than 6 characters')
-    }
+    var name = document.getElementById('name')
+    var form = document.getElementById('form')
+    name.addEventListener('blur', function(e){
+        if (e.target.value.length < 6){
+            document.getElementById('name-error').textContent = 'The name requires more than 6 characters'
+        }
+    })
 }
